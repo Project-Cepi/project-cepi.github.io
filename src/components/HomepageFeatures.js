bcdec9ed52786aa1b9b630bcff95ab7525a9ae10
LeoDog896 <LeoDog896@hotmail.com>
2021-10-29 12:25:43 +0000

    Add installation

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -2,58 +2,33 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Cepi's tooling and API is for anyone to be able to use it.
-      </>
-    ),
-  },
-  {
-    title: 'Flexible',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Cepi's tools allow you to fit any number of situations and needs.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by Minestom',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        A blazing fast server framework means you don't have to worry about performance issues.
-      </>
-    ),
-  },
-];
-
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+			<div>
+				<h1>Installation</h1>
+				<p>
+					If you want to mess with tools in your <b>own</b> server (cepi.world is the main instance),
+					you can boot up your own instance:
+				</p>
+				<p>
+					Shell (Mac, Linux):
+					<br />
+					<code>
+						curl -fsSL https://raw.githubusercontent.com/Project-Cepi/localhost/main/install.sh | sh
+					</code>
+				</p>
+				<br/>
+				<p>
+					Powershell (Windows):
+					<br />
+					<code>
+						iwr "https://raw.githubusercontent.com/Project-Cepi/localhost/main/install.ps1" | iex
+					</code>
+				</p>
+			</div>
         </div>
       </div>
     </section>
